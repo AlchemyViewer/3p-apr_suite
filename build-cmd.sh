383ac2d95b0c1e9416c9cca3fb1fae810875fa64
Rye Mutt <rye@alchemyviewer.org>
2024-06-16 11:53:59 -0400

    Build script cleanup

--- build-cmd.sh
+++ build-cmd.sh
@@ -82,7 +82,7 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
     which nmake
 
     for proj in apr aprutil apriconv
-      do build_sln "apr-util/aprutil.sln" "Debug|$AUTOBUILD_WIN_VSPLATFORM" "$proj"
+      do msbuild.exe $(cygpath -w 'apr-util/aprutil.sln') /t:"$proj" /p:Configuration=Debug /p:Platform=$AUTOBUILD_WIN_VSPLATFORM
     done
 
     mkdir -p "$DEBUG_OUT_DIR" || echo "$DEBUG_OUT_DIR exists"
@@ -92,7 +92,7 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
     cp "apr-util$bitdir/Debug/aprutil-1.lib" "$DEBUG_OUT_DIR"
 
     for proj in apr aprutil apriconv
-      do build_sln "apr-util/aprutil.sln" "Release|$AUTOBUILD_WIN_VSPLATFORM" "$proj"
+      do msbuild.exe $(cygpath -w 'apr-util/aprutil.sln') /t:"$proj" /p:Configuration=Release /p:Platform=$AUTOBUILD_WIN_VSPLATFORM
     done
 
     mkdir -p "$RELEASE_OUT_DIR" || echo "$RELEASE_OUT_DIR exists"
@@ -115,61 +115,33 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
   ;;
 
   darwin*)
-    # Setup osx sdk platform
-    SDKNAME="macosx"
-    export SDKROOT=$(xcodebuild -version -sdk ${SDKNAME} Path)
-
-    # Deploy Targets
-    X86_DEPLOY=10.15
-    ARM64_DEPLOY=11.0
-
     # Setup build flags
-    ARCH_FLAGS_X86="-arch x86_64 -mmacosx-version-min=${X86_DEPLOY} -isysroot ${SDKROOT}"
-    ARCH_FLAGS_ARM64="-arch arm64 -mmacosx-version-min=${ARM64_DEPLOY} -isysroot ${SDKROOT}"
-    DEBUG_COMMON_FLAGS="-O0 -g -fPIC -DPIC"
-    RELEASE_COMMON_FLAGS="-O3 -g -fPIC -DPIC -fstack-protector-strong"
-    DEBUG_CFLAGS="$DEBUG_COMMON_FLAGS"
-    RELEASE_CFLAGS="$RELEASE_COMMON_FLAGS"
-    DEBUG_CXXFLAGS="$DEBUG_COMMON_FLAGS -std=c++17"
-    RELEASE_CXXFLAGS="$RELEASE_COMMON_FLAGS -std=c++17"
-    DEBUG_CPPFLAGS="-DPIC"
-    RELEASE_CPPFLAGS="-DPIC"
-    DEBUG_LDFLAGS="-Wl,-headerpad_max_install_names"
-    RELEASE_LDFLAGS="-Wl,-headerpad_max_install_names"
+    C_OPTS_X86="-arch x86_64 $LL_BUILD_RELEASE_CFLAGS"
+    C_OPTS_ARM64="-arch arm64 $LL_BUILD_RELEASE_CFLAGS"
+    CXX_OPTS_X86="-arch x86_64 $LL_BUILD_RELEASE_CXXFLAGS"
+    CXX_OPTS_ARM64="-arch arm64 $LL_BUILD_RELEASE_CXXFLAGS"
+    LINK_OPTS_X86="-arch x86_64 $LL_BUILD_RELEASE_LINKER"
+    LINK_OPTS_ARM64="-arch arm64 $LL_BUILD_RELEASE_LINKER"
 
-    # x86 Deploy Target
-    export MACOSX_DEPLOYMENT_TARGET=${X86_DEPLOY}
+    # deploy target
+    export MACOSX_DEPLOYMENT_TARGET=${LL_BUILD_DARWIN_BASE_DEPLOY_TARGET}
 
     PREFIX="$STAGING_DIR"
-    PREFIX_DEBUG_X86="$PREFIX/temp_debug_x86"
-    PREFIX_DEBUG_ARM64="$PREFIX/temp_debug_arm64"
     PREFIX_RELEASE_X86="$PREFIX/temp_release_x86"
     PREFIX_RELEASE_ARM64="$PREFIX/temp_release_arm64"
 
-    mkdir -p $PREFIX_DEBUG_X86
-    mkdir -p $PREFIX_DEBUG_ARM64
     mkdir -p $PREFIX_RELEASE_X86
     mkdir -p $PREFIX_RELEASE_ARM64
+
+    # ARM64 Deploy Target
+    export MACOSX_DEPLOYMENT_TARGET=${ARM64_DEPLOY}
 
     pushd "$TOP_DIR/apr"
         autoreconf -fvi
 
-        mkdir -p "build_debug_x86"
-        pushd "build_debug_x86"
-            CFLAGS="$ARCH_FLAGS_X86 $DEBUG_CFLAGS -msse4.2" CXXFLAGS="$ARCH_FLAGS_X86 $DEBUG_CXXFLAGS -msse4.2" LDFLAGS="$ARCH_FLAGS_X86 $DEBUG_LDFLAGS" \
-                ../configure --enable-debug --disable-shared --enable-static --prefix="$PREFIX_DEBUG_X86" --host=x86_64-apple-darwin
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # conditionally run unit tests
-            # if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-            #     make check
-            # fi
-        popd
-
         mkdir -p "build_release_x86"
         pushd "build_release_x86"
-            CFLAGS="$ARCH_FLAGS_X86 $RELEASE_CFLAGS -msse4.2" CXXFLAGS="$ARCH_FLAGS_X86 $RELEASE_CXXFLAGS -msse4.2" LDFLAGS="$ARCH_FLAGS_X86 $RELEASE_LDFLAGS" \
+            CFLAGS="$C_OPTS_X86" CXXFLAGS="$CXX_OPTS_X86" LDFLAGS="$LINK_OPTS_X86" \
                 ../configure --disable-shared --enable-static --prefix="$PREFIX_RELEASE_X86" --host=x86_64-apple-darwin
             make -j$AUTOBUILD_CPU_COUNT
             make install
@@ -180,25 +152,11 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
             # fi
         popd
 
-        # ARM64 Deploy Target
-        export MACOSX_DEPLOYMENT_TARGET=${ARM64_DEPLOY}
 
-        mkdir -p "build_debug_arm64"
-        pushd "build_debug_arm64"
-            CFLAGS="$ARCH_FLAGS_ARM64 $DEBUG_CFLAGS" CXXFLAGS="$ARCH_FLAGS_ARM64 $DEBUG_CXXFLAGS" LDFLAGS="$ARCH_FLAGS_ARM64 $DEBUG_LDFLAGS" \
-                ../configure --enable-debug --disable-shared --enable-static --prefix="$PREFIX_DEBUG_ARM64" --host=aarch64-apple-darwin
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # conditionally run unit tests
-            # if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-            #     make check
-            # fi
-        popd
 
         mkdir -p "build_release_arm64"
         pushd "build_release_arm64"
-            CFLAGS="$ARCH_FLAGS_ARM64 $RELEASE_CFLAGS" CXXFLAGS="$ARCH_FLAGS_ARM64 $RELEASE_CXXFLAGS" LDFLAGS="$ARCH_FLAGS_ARM64 $RELEASE_LDFLAGS" \
+            CFLAGS="$C_OPTS_ARM64" CXXFLAGS="$CXX_OPTS_ARM64" LDFLAGS="$LINK_OPTS_ARM64" \
                 ../configure --disable-shared --enable-static --prefix="$PREFIX_RELEASE_ARM64" --host=aarch64-apple-darwin
             make -j$AUTOBUILD_CPU_COUNT
             make install
@@ -210,12 +168,6 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
         popd        
     popd
 
-    # pushd "$PREFIX_DEBUG/lib"
-    #     fix_dylib_id "libapr-1.dylib"
-    #     dsymutil libapr-*.*.dylib
-    #     strip -x -S libapr-*.*.dylib
-    # popd
-
     # pushd "$PREFIX_RELEASE/lib"
     #     fix_dylib_id "libapr-1.dylib"
     #     dsymutil libapr-*.*.dylib
@@ -225,33 +177,11 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
     pushd "$TOP_DIR/apr-util"
         autoreconf -fvi
 
-        # x86_64 Deploy Target
-        export MACOSX_DEPLOYMENT_TARGET=${X86_DEPLOY}
-
-        mkdir -p "build_debug_x86"
-        pushd "build_debug_x86"
-            cp -a $STAGING_DIR/packages/lib/debug/*.a $STAGING_DIR/packages/lib
-
-            CFLAGS="$ARCH_FLAGS_X86 $DEBUG_CFLAGS -msse4.2" CXXFLAGS="$ARCH_FLAGS_X86 $DEBUG_CXXFLAGS -msse4.2" LDFLAGS="$ARCH_FLAGS_X86 $DEBUG_LDFLAGS" \
-                ../configure --prefix="$PREFIX_DEBUG_X86" --with-apr="$PREFIX_DEBUG_X86" \
-                --with-expat="$PREFIX/packages" --disable-shared --enable-static --host=x86_64-apple-darwin
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # conditionally run unit tests
-            # if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-            #     export DYLD_LIBRARY_PATH="$STAGING_DIR/packages/lib"
-            #     make check
-            # fi
-
-            rm $STAGING_DIR/packages/lib/*.a
-        popd
-
         mkdir -p "build_release_x86"
         pushd "build_release_x86"
             cp -a $STAGING_DIR/packages/lib/release/*.a $STAGING_DIR/packages/lib
 
-            CFLAGS="$ARCH_FLAGS_X86 $RELEASE_CFLAGS -msse4.2" CXXFLAGS="$ARCH_FLAGS_X86 $RELEASE_CXXFLAGS -msse4.2" LDFLAGS="$ARCH_FLAGS_X86 $RELEASE_LDFLAGS" \
+            CFLAGS="$C_OPTS_X86" CXXFLAGS="$CXX_OPTS_X86" LDFLAGS="$LINK_OPTS_X86" \
                 ../configure --prefix="$PREFIX_RELEASE_X86" --with-apr="$PREFIX_RELEASE_X86" \
                 --with-expat="$PREFIX/packages" --disable-shared --enable-static --host=x86_64-apple-darwin
             make -j$AUTOBUILD_CPU_COUNT
@@ -266,33 +196,11 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
             rm $STAGING_DIR/packages/lib/*.a
         popd
 
-        # ARM64 Deploy Target
-        export MACOSX_DEPLOYMENT_TARGET=${ARM64_DEPLOY}
-
-        mkdir -p "build_debug_arm64"
-        pushd "build_debug_arm64"
-            cp -a $STAGING_DIR/packages/lib/debug/*.a $STAGING_DIR/packages/lib
-
-            CFLAGS="$ARCH_FLAGS_ARM64 $DEBUG_CFLAGS" CXXFLAGS="$ARCH_FLAGS_ARM64 $DEBUG_CXXFLAGS" LDFLAGS="$ARCH_FLAGS_ARM64 $DEBUG_LDFLAGS" \
-                ../configure --prefix="$PREFIX_DEBUG_ARM64" --with-apr="$PREFIX_DEBUG_ARM64" \
-                --with-expat="$PREFIX/packages" --disable-shared --enable-static --host=aarch64-apple-darwin
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # conditionally run unit tests
-            # if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-            #     export DYLD_LIBRARY_PATH="$STAGING_DIR/packages/lib"
-            #     make check
-            # fi
-
-            rm $STAGING_DIR/packages/lib/*.a
-        popd
-
         mkdir -p "build_release_arm64"
         pushd "build_release_arm64"
             cp -a $STAGING_DIR/packages/lib/release/*.a $STAGING_DIR/packages/lib
 
-            CFLAGS="$ARCH_FLAGS_ARM64 $RELEASE_CFLAGS" CXXFLAGS="$ARCH_FLAGS_ARM64 $RELEASE_CXXFLAGS" LDFLAGS="$ARCH_FLAGS_ARM64 $RELEASE_LDFLAGS" \
+            CFLAGS="$C_OPTS_ARM64" CXXFLAGS="$CXX_OPTS_ARM64" LDFLAGS="$LINK_OPTS_ARM64" \
                 ../configure --prefix="$PREFIX_RELEASE_ARM64" --with-apr="$PREFIX_RELEASE_ARM64" \
                 --with-expat="$PREFIX/packages" --disable-shared --enable-static --host=aarch64-apple-darwin
             make -j$AUTOBUILD_CPU_COUNT
@@ -309,12 +217,9 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
     popd
 
     mkdir -p "$PREFIX/include"
-    mkdir -p "$PREFIX/lib/debug"
     mkdir -p "$PREFIX/lib/release"
 
     # create fat libraries
-    lipo -create ${STAGING_DIR}/temp_debug_x86/lib/libapr-1.a ${STAGING_DIR}/temp_debug_arm64/lib/libapr-1.a -output ${STAGING_DIR}/lib/debug/libapr-1.a
-    lipo -create ${STAGING_DIR}/temp_debug_x86/lib/libaprutil-1.a ${STAGING_DIR}/temp_debug_arm64/lib/libaprutil-1.a -output ${STAGING_DIR}/lib/debug/libaprutil-1.a
     lipo -create ${STAGING_DIR}/temp_release_x86/lib/libapr-1.a ${STAGING_DIR}/temp_release_arm64/lib/libapr-1.a -output ${STAGING_DIR}/lib/release/libapr-1.a
     lipo -create ${STAGING_DIR}/temp_release_x86/lib/libaprutil-1.a ${STAGING_DIR}/temp_release_arm64/lib/libaprutil-1.a -output ${STAGING_DIR}/lib/release/libaprutil-1.a
 
@@ -323,20 +228,24 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
   ;;
 
   linux*)
-    # Default target per --address-size
-    opts="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE}"
+    # Linux build environment at Linden comes pre-polluted with stuff that can
+    # seriously damage 3rd-party builds.  Environmental garbage you can expect
+    # includes:
+    #
+    #    DISTCC_POTENTIAL_HOSTS     arch           root        CXXFLAGS
+    #    DISTCC_LOCATION            top            branch      CC
+    #    DISTCC_HOSTS               build_name     suffix      CXX
+    #    LSDISTCC_ARGS              repo           prefix      CFLAGS
+    #    cxx_version                AUTOBUILD      SIGN        CPPFLAGS
+    #
+    # So, clear out bits that shouldn't affect our configure-directed build
+    # but which do nonetheless.
+    #
+    unset DISTCC_HOSTS CFLAGS CPPFLAGS CXXFLAGS
 
-    # Setup build flags
-    DEBUG_COMMON_FLAGS="$opts -Og -g -fPIC -DPIC"
-    RELEASE_COMMON_FLAGS="$opts -O3 -g -fPIC -DPIC -fstack-protector-strong -D_FORTIFY_SOURCE=2"
-    DEBUG_CFLAGS="$DEBUG_COMMON_FLAGS"
-    RELEASE_CFLAGS="$RELEASE_COMMON_FLAGS"
-    DEBUG_CXXFLAGS="$DEBUG_COMMON_FLAGS -std=c++17"
-    RELEASE_CXXFLAGS="$RELEASE_COMMON_FLAGS -std=c++17"
-    DEBUG_CPPFLAGS="-DPIC"
-    RELEASE_CPPFLAGS="-DPIC"
-    DEBUG_LDFLAGS="$opts"
-    RELEASE_LDFLAGS="$opts"      
+    # Default target per --address-size
+    opts_c="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE_CFLAGS}"
+    opts_cxx="${TARGET_OPTS:--m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE_CXXFLAGS}"
 
     # Handle any deliberate platform targeting
     if [ -z "${TARGET_CPPFLAGS:-}" ]; then
@@ -348,31 +257,16 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
     fi
 
     PREFIX="$STAGING_DIR"
-    PREFIX_DEBUG="$PREFIX/temp_debug"
     PREFIX_RELEASE="$PREFIX/temp_release"
 
-    mkdir -p $PREFIX_DEBUG
     mkdir -p $PREFIX_RELEASE
 
     pushd "$TOP_DIR/apr"
         autoreconf -fvi
 
-        mkdir -p "build_debug"
-        pushd "build_debug"
-            CFLAGS="$DEBUG_CFLAGS" CXXFLAGS="$DEBUG_CXXFLAGS" LDFLAGS="$DEBUG_LDFLAGS" \
-                ../configure --enable-debug --disable-shared --enable-static --prefix="$PREFIX_DEBUG"
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # conditionally run unit tests
-            # if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-            #     make check
-            # fi
-        popd
-
         mkdir -p "build_release"
         pushd "build_release"
-            CFLAGS="$RELEASE_CFLAGS" CXXFLAGS="$RELEASE_CXXFLAGS" LDFLAGS="$RELEASE_LDFLAGS" \
+            CFLAGS="$opts_c" CXXFLAGS="$opts_cxx" \
                 ../configure --disable-shared --enable-static --prefix="$PREFIX_RELEASE"
             make -j$AUTOBUILD_CPU_COUNT
             make install
@@ -389,34 +283,12 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
 
         cp -a $STAGING_DIR/packages/include/expat/*.h $STAGING_DIR/packages/include/
 
-        mkdir -p "build_debug"
-        pushd "build_debug"
-            cp -a $STAGING_DIR/packages/lib/debug/*.a $STAGING_DIR/packages/lib
-
-            CFLAGS="$DEBUG_CFLAGS -L$STAGING_DIR/packages/include" \
-            CXXFLAGS="$DEBUG_CXXFLAGS -L$STAGING_DIR/packages/include" \
-            LDFLAGS="$DEBUG_LDFLAGS" \
-                ../configure --prefix="$PREFIX_DEBUG" --with-apr="$PREFIX_DEBUG" \
-                --with-expat="$PREFIX/packages" --without-crypto --disable-shared --enable-static
-            make -j$AUTOBUILD_CPU_COUNT
-            make install
-
-            # conditionally run unit tests
-            # if [ "${DISABLE_UNIT_TESTS:-0}" = "0" ]; then
-            #     export LD_LIBRARY_PATH="$STAGING_DIR/packages/lib"
-            #     make check
-            # fi
-
-            rm $STAGING_DIR/packages/lib/*.a
-        popd
-
         mkdir -p "build_release"
         pushd "build_release"
             cp -a $STAGING_DIR/packages/lib/release/*.a $STAGING_DIR/packages/lib
 
-            CFLAGS="$RELEASE_CFLAGS -L$STAGING_DIR/packages/include" \
-            CXXFLAGS="$RELEASE_CXXFLAGS -L$STAGING_DIR/packages/include" \
-            LDFLAGS="$RELEASE_LDFLAGS" \
+            CFLAGS="$opts_c -L$STAGING_DIR/packages/include" \
+            CXXFLAGS="$opts_cxx -L$STAGING_DIR/packages/include" \
                 ../configure --prefix="$PREFIX_RELEASE" --with-apr="$PREFIX_RELEASE" \
                 --with-expat="$PREFIX/packages" --without-crypto --disable-shared --enable-static
             make -j$AUTOBUILD_CPU_COUNT
@@ -433,12 +305,9 @@ if not any(frag in d for frag in ('CommonExtensions', 'VSPerfCollectionTools', '
     popd
 
     mkdir -p "$PREFIX/include"
-    mkdir -p "$PREFIX/lib/debug"
     mkdir -p "$PREFIX/lib/release"
 
-    cp -a $PREFIX_DEBUG/lib/*.a $PREFIX/lib/debug
     cp -a $PREFIX_RELEASE/lib/*.a $PREFIX/lib/release
-
     cp -a $PREFIX_RELEASE/include/* $PREFIX/include/
   ;;
 esac
